d3f8746956a7ee39de043e14c74f3d212a37d1ab
TheComputationalCore <dineshchandra962@gmail.com>
2026-03-05 08:41:44 +0530

    Update BusServiceTest.java

--- src/test/java/com/busbooking/bus_booking_system/service/BusServiceTest.java
+++ src/test/java/com/busbooking/bus_booking_system/service/BusServiceTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class BusServiceTest {
+class BusServiceTest extends IntegrationTestBase{
 
     @InjectMocks
     private BusService busService;
