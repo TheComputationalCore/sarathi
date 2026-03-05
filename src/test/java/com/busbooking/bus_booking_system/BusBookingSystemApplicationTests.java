fde4f1eaf9e7bd2928f3a8834e3aa4aa4831be34
TheComputationalCore <dineshchandra962@gmail.com>
2026-03-05 08:42:23 +0530

    Update BusBookingSystemApplicationTests.java

--- src/test/java/com/busbooking/bus_booking_system/BusBookingSystemApplicationTests.java
+++ src/test/java/com/busbooking/bus_booking_system/BusBookingSystemApplicationTests.java
@@ -3,7 +3,7 @@ package com.busbooking.bus_booking_system;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class BusBookingSystemApplicationTests {
+class BusBookingSystemApplicationTests extends IntegrationTestBase{
 
 	@Test
 	void applicationClassIsPresent() {
